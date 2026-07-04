f370b8c76545b3d9317b9cb8e61b89c7251d3c32
POHTHONG <109568219+pttangent@users.noreply.github.com>
2026-07-04 09:03:34 +0800

    add package API

---
+++ src/graphfactorfactory/__init__.py
@@ -0,0 +1,4 @@
+from .core import BuildConfig, LAYERS, MultilayerGraphBuilder, audit_pit_frame, build_forward_labels, build_point_in_time_panel
+from .qlib_adapter import GraphFactorDataLoader, discover_numeric_features, export_qlib_table
+
+__version__ = "0.2.0"
