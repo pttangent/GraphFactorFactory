4bc5bc2608e6577f398e5c72853dc3db468be115
POHTHONG <109568219+pttangent@users.noreply.github.com>
2026-07-04 16:17:33 +0800

    add modular theme discovery package

---
+++ src/graphfactorfactory/themes/__init__.py
@@ -0,0 +1,4 @@
+from .models import ThemeCandidate, LifecycleRecord, SemanticLabel
+from .pipeline import ThemeDiscoveryPipeline, ThemeDiscoveryConfig
+
+__all__ = ["ThemeCandidate", "LifecycleRecord", "SemanticLabel", "ThemeDiscoveryPipeline", "ThemeDiscoveryConfig"]
